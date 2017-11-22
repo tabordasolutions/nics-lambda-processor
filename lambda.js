362b1006ca21533ef07ffbd45747ca966559ee62
Terry Weaver <terry.weaver@tabordasolutions.com>
2017-11-22 09:21:12 -0800

    Added some lambda logging statements. Changed db connection behavior when there is nothing to upsert.

--- lambda.js
+++ lambda.js
@@ -5,7 +5,9 @@ const moment = require('moment');
 let {dbconnectionparams, skyconnectparams} = require('./connections'); //default connections.
 
 let handler = function(event, context, callback) {
+    console.log('Checking Env Variables.');
     if ( dbconnectionparams.passworddecrypted && skyconnectparams.passworddecrypted ) {
+        console.log('Calling etl process...');
         processor.etlskyconnectdata({dboptions: dbconnectionparams,skyconnectoptions: skyconnectparams})
             .then(result => processor.prunestaledata(moment(result.timestamp).subtract(7, 'days'),dbconnectionparams))
             .then(result => console.log(result + '. Done.'))
@@ -14,7 +16,9 @@ let handler = function(event, context, callback) {
                 callback(e);
             });
     } else {
+        console.log('decrypting secrets...');
         secretsPromise.then(() => {
+            console.log('decrypted secrets');
             dbconnectionparams.password = process.env.PGPASSWORD;
             dbconnectionparams.passworddecrypted = true;
             skyconnectparams.password = process.env.SKYCONNECTPASSWORD;
